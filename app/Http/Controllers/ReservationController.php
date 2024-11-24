d359c8f480e84a9d99ee16fd4b69be67dd33eb97
Amirsajjad <Amirwebsajjad@gmail.com>
2024-11-25 00:36:44 +0330

    the reservation has been completed !

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,62 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;    
 
 class ReservationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
+        $reservation = new Reservation;
+        $reservation->create([
+            "user_id" => "RLdS1thW19u9fqwn",
+            "reservation_date_time" => "16/4/1402/003htn",
+            "number_of_people" => 12,
+            "status" => "payed"
+        ]);
+        return "nice";
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function get_user_reservations($user_id)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Reservation $reservation)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Reservation $reservation)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Reservation $reservation)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Reservation $reservation)
-    {
-        //
+        $reservation = Reservation::where("user_id", $user_id)->first();
+        if(!$reservation)
+        {
+            return response()->json(["message" => "هیچ رزروی برای این کاربر یافت نشد!"],404);
+        }
+        return response()->json(["data" => $reservation],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\FoodController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderDetailController;
+use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -35,6 +36,9 @@ Route::get("/food/create" , [FoodController::class, "create"]);
 Route::get("order/create", [OrderController::class , "create"]);
 Route::get("order/{order_id}", [OrderController::class , "get_orders"]);
 Route::get("order_detail/create", [OrderDetailController::class , "create"]);
+// ============================================================================================
+Route::get("/create/reservation", [ReservationController:: class , "create"]);
+Route::get("/reservation/{user_id}", [ReservationController:: class , "get_user_reservations"]);
 
 
 
